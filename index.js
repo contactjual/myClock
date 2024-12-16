bb9c803b9dbab4f2bdfc70ca476f6973bfefc255
Jual Rana <contactjual@gmail.com>
2024-12-16 23:04:36 +0600

    all done without day

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-// alert("The app is updating now")
+function secManage() {
+    let myDate = new Date();   
 
-function timeManage () {
-    const getHourElement = document.getElementById('time-hour');
-    const getMinElement = document.getElementById('time-min');
+    // sec 
     const getSecElement = document.getElementById('time-sec');
+    getSecElement.innerText = myDate.getSeconds(2);
 
-    
-}
+    // min 
+    const getMinElement = document.getElementById('time-min');
+    getMinElement.innerText = myDate.getMinutes();
+
+    // hour 
+    const getHourElement = document.getElementById('time-hour');
+    getHourElement.innerText = myDate.getHours();
+
+    // day
+    const getDayElement = document.getElementById('date-day');
+    getDayElement.innerText = myDate.getDate();
+
+    // month
+    const getMonthElement = document.getElementById('date-month');
+    const myMonths = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    let monthIndex = myDate.getMonth();
+    getMonthElement.innerText = myMonths[monthIndex];
+
+    // year
+    const getYeatElement = document.getElementById('date-year');
+    getYeatElement.innerText = myDate.getFullYear();
+};
+
+setInterval(secManage, 1000);
+
