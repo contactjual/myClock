e9bd336aac653abfc28f3af70312d783d849a826
Jual Rana <contactjual@gmail.com>
2024-12-16 22:12:20 +0600

    design done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-alert("The app is updating now")
+// alert("The app is updating now")
+
+function timeManage () {
+    const getHourElement = document.getElementById('time-hour');
+    const getMinElement = document.getElementById('time-min');
+    const getSecElement = document.getElementById('time-sec');
+
+    
+}
